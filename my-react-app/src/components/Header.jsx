8eedda3b15cb5c0cdb860799d1b14504e983de04
Meron Hailu <hailumeron03@gmail.com>
2026-01-15 19:56:22 -0500

    react app

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <header>My favorite code</header>;
+}
+
+export default Header;
---
+++ my-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello this is meron</h1>
+      <p>i am react developer</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
